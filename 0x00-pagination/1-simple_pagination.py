10fc3ed6a980a0e13be1949bff1c518ae69d33d4
unknown <jspacific777@gmail.com>
2023-07-25 03:38:51 +0100

    helper pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -30,6 +30,7 @@ class Server:
 
         return self.__dataset
 
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         check for instance and return the data
